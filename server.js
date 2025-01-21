a77a38c933fc1f0684d29371ac61a31abc8ce39f
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-20 21:10:53 -0600

    Update the users and posts responses

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 // Creating the PORT constant and assign the process environment or a default PORT# 3000
 const PORT = process.env.PORT || 3000 ;
 
+// ------------------------- Routers Imports
+// users Import
 const users = require('./data/users.js');
+// Posts Import
 const posts = require('./data/posts.js');
 
 
@@ -15,25 +18,32 @@ const posts = require('./data/posts.js');
 
 
 // Users requests
-    app.get("/users/:name", (req, res) => {
+    app.get("/users/:id", (req, res, next) => {
 
-            //res.json(users);
-            const user = users.find((user) => user.name == req.params.name);
-            if (user) {res.json(user)} 
+            // res.json(users);
+            const user = users.find((user) => user.id == req.params.id);
+            if (user) {
+                res.json(user) ;
+                next();
+            } 
             else if (!user) {
                 res.statusCode = 404;
-                res.send(`Error ${res.statusCode} User Not Found`)
+                res.send(`Error ${res.statusCode}: User ${req.params.id} Not Found`);
+                next();
             };
     });
 
 
 // Posts requests    
-    app.get("/posts/:id", (req, res) => {
+    app.get("/posts/:id", (req, res, next) => {
         const post = posts.find((post) => post.id == req.params.id);
         if (post) {
             res.json(post);
+            next();
         } else if (!post) {
-            res.send("Post not found");
+            res.statusCode = 404;
+            res.send(`Error ${res.statusCode}: User ${req.params.id} Not Found`);
+            next();
         }
         
     });
